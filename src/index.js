e86ca8630dd548309a5ea05b52de1d6311a9eca3
Vladislav <ivaskevichvladislav@gmail.com>
2021-03-10 00:23:15 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = {};
+  let stack = [];
+  for (p of bracketsConfig) {
+    config[p[0]] = p[1];
+  }
+  for (letter of str) {
+    if (config[stack[stack.length - 1]] === letter) {
+      stack.pop();      
+    } else {
+      stack.push(letter);
+    }
+  }
+  if (stack.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
